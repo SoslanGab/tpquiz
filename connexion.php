81412639f9a70f1ae9bc19ea5fdc76668a299c78
soslan <so.gabarayev@gmail.com>
2023-07-04 16:31:40 +0200

    ajout fonction pour ce connecter et si utilisateur existe pas le cree et ajout quizz avec css

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php 
 
     try {
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=tpquizz', 'root');
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=tpquizz', 'root');
     } catch (Exception $e) {
         echo 'Erreur de connexion : ' . $e->getMessage();
     }
--- traitement.php
+++ traitement.php
@@ -1,20 +1,30 @@
 <?php
+
 include 'connexion.php';
 
-$nickname = $_POST ['nickname'];
-$respons = $_POST ['respons'];
 
-$query = $bdd->prepare("SELECT id FROM user WHERE nickname = :nickname");
-$query->execute ([':nickname' => $nickname]);
-$user = $query->fetch(PDO::FETCH_ASSOC); 
+$query = $bdd->prepare("SELECT * FROM user");
+$query->execute();
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
 
-if ($user){
-    $userid = $user ['id'];
-} else {
 
-$insertQuery = $bdd->prepare("INSERT INTO user (nickname) values (:nickname)");
-$insertQuery->execute([':nickname' => $nickname]);
-$userid = $bdd->lastInsertId();
+if (isset($_POST['connect'])) {
+    $nickname = $_POST['nickname'];
+
+    $query = $bdd->prepare("SELECT id FROM user WHERE nickname = :nickname");
+    $query->execute([':nickname' => $nickname]);
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        $userid = $user['id'];
+    } else {
+        $insertQuery = $bdd->prepare("INSERT INTO user (nickname) VALUES (:nickname)");
+        $insertQuery->execute([':nickname' => $nickname]);
+        $userid = $bdd->lastInsertId();
+    }
+
+    header("location: quizz.php");
 }
 
-header ("location:accueil.php");
+
+
