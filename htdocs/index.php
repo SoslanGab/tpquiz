0c66b1aa7a7569e2548e992c7fe540e1b26a4029
Melyne99 <melyne.marques1999@gmail.com>
2023-07-06 13:48:23 +0200

    code a jour

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,56 +8,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <title>Quizz</title>
+
+    <style>
+       body,
+    html {
+        margin: 0px;
+        padding: 0px;
+        position: relative;
+        min-height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    body {
+        background-image: url('https://external-preview.redd.it/Tf2MC-orVRAI8OL5tjyF4k4H2Q6QLHTWllekGfqdt3c.png?width=960&crop=smart&auto=webp&s=be4ab9a2fa3ca2f7011799377f118a8dbc64aacb');
+        background-size: cover;
+    }
+
+    canvas {
+        filter: blur(1px);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+
+    #s_connexion {
+        position: relative;
+        z-index: 1;
+    }
+    </style>
 </head>
 <body>
-    
+<canvas id="canvas"></canvas>
 
 <section id="s_connexion" class="vh-100 s_connexion">
-  <div class="container  h-100">
-    <div class="row d-flex align-items-center justify-content-center h-100">
-      <div class="col-md-8 col-lg-7 col-xl-6">
-        <img src="image/quizzimg.png"
-          class="img-fluid" alt="image">
-      </div>
-      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-        <form action="traitement.php" method="POST">
-          <!-- Email input -->
-          <div class="form-outline mb-4">
-            <input name="nickname" type="text" id="form1Example13" class="form-control form-control-lg" />
-            <label class="form-label" for="form1Example13">Login</label>
-          </div>
-          <!-- Submit button -->
-          <button name="connect" type="submit" class="btn btn-primary btn-lg btn-block">Se Connecter</button>
+    <div class="container  h-150">
+        <div class="row d-flex align-items-center justify-content-center h-100">
+            <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                <form action="traitement.php" method="POST">
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                        <input name="nickname" type="text" id="form1Example13"
+                               class="form-control form-control-lg"/>
+                        <label class="form-label" for="form1Example13">Login</label>
+                    </div>
 
-          <div class="divider d-flex align-items-center my-4">
-            <p class="text-center fw-bold mx-3 mb-0 text-muted">Suivez-nous</p>
-          </div>
+                    <!-- Submit button -->
+                    <button name="connect" type="submit" class="btn btn-primary btn-lg btn-block">Se Connecter
+                    </button>
 
-          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="#!"
-            role="button">
-            <i class="fab fa-facebook-f me-2"></i>
-          </a>
-          <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!"
-            role="button">
-            <i class="fab fa-twitter me-2"></i></a>
-        </form>
-      </div>
+                    <div class="divider d-flex align-items-center my-4">
+                        <p class="text-center fw-bold mx-3 mb-0 text-muted">Suivez-nous</p>
+                    </div>
+
+                    <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="#!"
+                       role="button">
+                        <i class="fab fa-facebook-f me-2"></i>
+                    </a>
+                    <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!"
+                       role="button">
+                        <i class="fab fa-twitter me-2"></i></a>
+                </form>
+            </div>
+            <div class="col-md-8 col-lg-7 col-xl-6">
+                <img src="image/quizzimg.png"
+                     class="img-fluid" alt="image">
+            </div>
+        </div>
     </div>
-  </div>  
 </section>
 
-
-
-toto est la
-
-mais je veux toto
-
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="script.js"></script>
-
-
+<script src="js/main.js"></script>
 </body>
 </html>
--- htdocs/quizz.php
+++ htdocs/quizz.php
@@ -1,3 +1,33 @@
+<?php
+include 'connexion.php';
+
+// Récupérer les questions depuis la base de données
+$query = "SELECT question, respons, option1, option2, option3 FROM quizz";
+$result = $bdd->query($query);
+
+// Stocker les questions et les options de réponse dans des tableaux
+$questions = array();
+$options = array();
+
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $questions[] = $row['question'];
+    $options[] = array(
+        'respons' => $row['respons'],
+        'options' => array(
+            $row['option1'],
+            $row['option2'],
+            $row['option3'],
+            $row['respons']
+        )
+    );
+}
+
+// Mélanger les options de réponse de chaque question
+foreach ($options as &$questionOptions) {
+    shuffle($questionOptions['options']);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,48 +35,128 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>quizz</title>
+    <title>Quizz</title>
+
+    <style>
+    body,
+    html {
+        margin: 0px;
+        padding: 0px;
+        position: relative;
+        min-height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    body {
+        background-image: url('https://external-preview.redd.it/Tf2MC-orVRAI8OL5tjyF4k4H2Q6QLHTWllekGfqdt3c.png?width=960&crop=smart&auto=webp&s=be4ab9a2fa3ca2f7011799377f118a8dbc64aacb');
+        background-size: cover;
+    }
+
+    canvas {
+        filter: blur(1px);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+</style>
+
 </head>
 <body>
-    
-
+    <canvas id="canvas"></canvas>
     <section class="s_connexion">
         <div class="container mt-sm-5 my-1">
-            <div class="question ml-sm-5 pl-sm-5 pt-2">
-                <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
-                <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
-                    <label class="options">Small Business Owner or 
-                        <input type="radio" name="radio">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="options">Nonprofit Owner or Employee
-                        <input type="radio" name="radio">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="options">Journalist or Activist
-                        <input type="radio" name="radio">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="options">Other
-                        <input type="radio" name="radio">
-                        <span class="checkmark"></span>
-                    </label>
-                </div>
-            </div>
+            <div id="question-container" class="question ml-sm-5 pl-sm-5 pt-2"></div>
             <div class="d-flex align-items-center pt-3">
                 <div id="prev">
-                    <button class="btn btn-primary">Precedent</button>
+                    <button class="btn btn-primary">Précédent</button>
                 </div>
                 <div class="ml-auto mr-sm-5">
-                    <button class="btn btn-success">Suivant</button>
+                    <button id="next" class="btn btn-success">Suivant</button>
                 </div>
             </div>
         </div>
     </section>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var questions = <?php echo json_encode($questions); ?>;
+            var options = <?php echo json_encode($options); ?>;
+            var currentQuestion = 0;
+            var userResponses = {};
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="script.js"></script>
+            // Afficher la première question
+            displayQuestion();
 
+            // Fonction pour afficher la question actuelle
+            function displayQuestion() {
+                var questionContainer = $("#question-container");
+                questionContainer.html('<div class="py-2 h5"><b>Question ' + (currentQuestion + 1) + ': ' + questions[currentQuestion] + '</b></div>');
+
+                // Récupérer les options de réponse pour la question actuelle
+                var currentOptions = options[currentQuestion].options;
+
+                // Afficher les options de réponse dans le formulaire
+                var optionsContainer = $('<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options"></div>');
+                for (var i = 0; i < currentOptions.length; i++) {
+                    var option = currentOptions[i];
+                    var label = $('<label class="options">' + option + '<input type="radio" name="radio" value="' + option + '"><span class="checkmark"></span></label>');
+                    optionsContainer.append(label);
+                }
+                questionContainer.append(optionsContainer);
+
+                questionContainer.append('<input type="hidden" id="selected-option" value=""/>');
+            }
+
+            // Événement lors du clic sur le bouton "Suivant"
+            $("#next").click(function() {
+                var selectedOption = $("input[name=radio]:checked").val();
+
+                // Valider la réponse sélectionnée
+                if (selectedOption) {
+                    // Enregistrer la réponse dans la base de données
+                    userResponses[questions[currentQuestion]] = selectedOption;
+                    $.post("save_response.php", { question: questions[currentQuestion], response: selectedOption, responsUser: options[currentQuestion].respons })
+                        .done(function(data) {
+                            console.log("Réponse enregistrée avec succès !");
+                        })
+                        .fail(function() {
+                            console.log("Erreur lors de l'enregistrement de la réponse.");
+                        });
+                }
+
+                // Passer à la question suivante
+                currentQuestion++;
+
+                // Vérifier s'il y a encore des questions à afficher
+                if (currentQuestion < questions.length) {
+                    displayQuestion();
+                } else {
+                    // Afficher un message de fin du quizz ou rediriger vers une autre page
+                    $("#question-container").html("Quizz terminé !");
+                }
+            });
+
+            // Événement lors du clic sur le bouton "Précédent"
+            $("#prev").click(function() {
+                // Passer à la question précédente
+                currentQuestion--;
+
+                // Vérifier si la question actuelle est inférieure à zéro
+                if (currentQuestion < 0) {
+                    // Si la question actuelle est inférieure à zéro, la remettre à zéro
+                    currentQuestion = 0;
+                }
+
+                // Afficher la question précédente
+                displayQuestion();
+            });
+        });
+    </script>
 </body>
 </html>
