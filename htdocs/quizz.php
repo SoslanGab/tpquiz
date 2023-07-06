3ed0de62b31d13ea81deb64b0041d46d245f60d3
Melyne99 <melyne.marques1999@gmail.com>
2023-07-06 15:31:53 +0200

    rajout  main js

--- htdocs/quizz.php
+++ htdocs/quizz.php
@@ -109,6 +109,7 @@ foreach ($options as &$questionOptions) {
 
 
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="js/main.js"></script>
     <script>
         $(document).ready(function() {
             var questions = <?php echo json_encode($questions); ?>;
