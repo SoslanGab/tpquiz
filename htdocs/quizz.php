190f559f98b5b9e6093753e0df17b0b56360d9a2
soslan <so.gabarayev@gmail.com>
2023-07-10 13:36:32 +0200

    comment effet arr plan

--- htdocs/quizz.php
+++ htdocs/quizz.php
@@ -38,8 +38,7 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 </head>
 <body>
 <!-- effets arrière plan -->
-
-<canvas id="canvas"></canvas>
+    <!-- <canvas id="canvas"></canvas> -->
 <!-- effets arrière plan -->
 
 <section class="s_connexion">
@@ -58,14 +57,13 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     <section class="s_connexion">
         <div class="container mt-sm-5 my-1">
             <div id="question-container" class="question ml-sm-5 pl-sm-5 pt-2"></div>
-            <div class="d-flex align-items-center pt-3">
-                <div id="prev">
-                    <button id="" class="btn btn-primary">Précédent</button>
+                <div class="d-flex align-items-center pt-3">
+                    <div id="prev">
+                        <button id="" class="btn btn-primary">Précédent</button>
+                    </div>
+                    <div class="ml-auto mr-sm-5">
+                        <button id="next" class="btn btn-success">Suivant</button>
                 </div>
-                <div class="ml-auto mr-sm-5">
-                    <button id="next" class="btn btn-success">Suivant</button>
-                </div>
-
             </div>
             <div id="next">
                 <a href="#" class="buttonnext">
@@ -81,180 +79,8 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="js/main.js"></script>
 <script>
-$(document).ready(function() {
-    var questions = <?php echo json_encode($questions); ?>;
-    var options = <?php echo json_encode($options); ?>;
-    var currentQuestion = 0;
-    var userResponses = {};
 
-    // Afficher la première question
-    displayQuestion();
-
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="js/main.js"></script>
-    <script>
-        $(document).ready(function() {
-            var questions = <?php echo json_encode($questions); ?>;
-            var options = <?php echo json_encode($options); ?>;
-            var currentQuestion = 0;
-            var userResponses = {};
-
-
-    // Fonction pour afficher la question actuelle
-    function displayQuestion() {
-        var questionContainer = $("#question-container");
-        questionContainer.html('<div class="py-2 h5"><b>Question ' + (currentQuestion + 1) + ': ' + questions[currentQuestion] + '</b></div>');
-
-        // Récupérer les options de réponse pour la question actuelle
-        var currentOptions = options[currentQuestion].options;
-
-        // Afficher les options de réponse dans le formulaire
-        var optionsContainer = $('<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options"></div>');
-        for (var i = 0; i < currentOptions.length; i++) {
-            var option = currentOptions[i];
-            var label = $('<label class="options">' + option + '<input type="radio" name="radio" value="' + option + '"><span class="checkmark"></span></label>');
-            optionsContainer.append(label);
-        }
-        questionContainer.append(optionsContainer);
-
-
-        questionContainer.append('<input type="hidden" id="selected-option" value=""/>');
-    }
-
-    // Événement lors du clic sur le bouton "Suivant"
-    $("#next").click(function(e) {
-        e.preventDefault();
-        var selectedOption = $("input[name=radio]:checked").val();
-
-        // Valider la réponse sélectionnée
-        if (selectedOption) {
-            // Enregistrer la réponse de l'utilisateur
-            var question = questions[currentQuestion];
-            var responsUser = options[currentQuestion].respons;
-
-            $.ajax({
-                url: "save_response.php",
-                method: "POST",
-                data: { question: question, response: selectedOption, responsUser: responsUser },
-                success: function(data) {
-                    console.log("Réponse enregistrée avec succès !");
-                    var response = JSON.parse(data);
-                    // ...
-                },
-                error: function() {
-                    console.log("Erreur lors de l'enregistrement de la réponse.");
-                }
-
-                // Afficher les options de réponse dans le formulaire
-                var optionsContainer = $('<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options"></div>');
-                for (var i = 0; i < currentOptions.length; i++) {
-                    var option = currentOptions[i];
-                    var label = $('<label class="options">' + option + '<input type="radio" name="radio" value="' + option + '" required><span class="checkmark"></span></label>');
-                    optionsContainer.append(label);
-                }
-
-                questionContainer.append(optionsContainer);
-
-                questionContainer.append('<input type="hidden" id="selected-option" value=""/>');
-            }
-
-            // Événement lors du clic sur le bouton "Suivant"
-                $("#next").click(function() {
-                    var selectedOption = $("input[name=radio]:checked").val();
-                    if (!selectedOption) {
-                        alert("Veuillez choisir une réponse.");
-                        return;
-                    }
-
-                    // Valider la réponse sélectionnée
-                    if (selectedOption) {
-                        // Enregistrer la réponse de l'utilisateur
-                        var question = questions[currentQuestion];
-                        var responsUser = options[currentQuestion].respons;
-
-                        $.ajax({
-                            url: "save_response.php",
-                            method: "POST",
-                            data: { question: question, response: selectedOption, responsUser: responsUser },
-                            success: function(data) {
-                                console.log("Réponse enregistrée avec succès !");
-                                var response = JSON.parse(data);
-                                // ...
-                            },
-                            error: function() {
-                                console.log("Erreur lors de l'enregistrement de la réponse.");
-                            }
-                        });
-                    }
-
-                    // Passer à la question suivante
-                    currentQuestion++;
-
-                    // Vérifier s'il y a encore des questions à afficher
-                    if (currentQuestion < questions.length) {
-                        displayQuestion();
-                    } else {
-                        // Afficher un message de fin du quizz ou rediriger vers une autre page
-                        $("#question-container").html("Quizz terminé !");
-                    }
-
-                    // Désactiver les options de réponse après avoir choisi une réponse
-                    $("#options input").attr("disabled", true);
-                });
-
-
-            // Événement lors du clic sur le bouton "Précédent"
-            $("#prev").click(function() {
-                // Passer à la question précédente
-                currentQuestion--;
-
-                // Vérifier si la question actuelle est inférieure à zéro
-                if (currentQuestion < 0) {
-                    // Si la question actuelle est inférieure à zéro, la remettre à zéro
-                    currentQuestion = 0;
-                }
-
-                // Afficher la question précédente
-                displayQuestion();
-
-            });
-        }
-
-
-        // Passer à la question suivante
-        currentQuestion++;
-
-        // Vérifier s'il y a encore des questions à afficher
-        if (currentQuestion < questions.length) {
-            displayQuestion();
-        } else {
-            // Afficher un message de fin du quizz ou rediriger vers une autre page
-            $("#question-container").html("Quizz terminé !");
-        }
-
-        // Désactiver les options de réponse après avoir choisi une réponse
-        $("#options input").attr("disabled", true);
-    });
-
-    // Événement lors du clic sur le bouton "Précédent"
-    $("#prev").click(function(e) {
-        e.preventDefault();
-        // Passer à la question précédente
-        currentQuestion--;
-
-        // Vérifier si la question actuelle est inférieure à zéro
-        if (currentQuestion < 0) {
-            // Si la question actuelle est inférieure à zéro, la remettre à zéro
-            currentQuestion = 0;
-        }
-
-        // Afficher la question précédente
-        displayQuestion();
-    });
-});
-</script>
-
-        $(document).ready(function() {
+    $(document).ready(function() {
         var questions = <?php echo json_encode($questions); ?>;
         var options = <?php echo json_encode($options); ?>;
         var currentQuestion = 0;
